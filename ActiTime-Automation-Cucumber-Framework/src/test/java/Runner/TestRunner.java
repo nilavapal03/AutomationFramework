bb4a5c8af754f1b49b39787c4cec95b8a8311898
NilavaPal <panilav03@gmail.com>
2020-04-22 19:27:52 +0530

    done

--- ActiTime-Automation-Cucumber-Framework/src/test/java/Runner/TestRunner.java
+++ ActiTime-Automation-Cucumber-Framework/src/test/java/Runner/TestRunner.java
@@ -6,12 +6,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="./resourceLib/Features",glue= {"StepDefination"}, 
-format= {"pretty","html:./report/cucumberReports",
-		"json:./report/cucumberReports/cucumber.json","junit:./report/cucumberReports/cucumber.xml"}
-		,tags= {"@SmokeTest"})
-
+@CucumberOptions(features="./resourceLib/Features", glue= {"StepDefination"},
+format= {"pretty","html:./report/cucumberReports"}, tags= {"@SmokeTest"})
 public class TestRunner {
-	
+
 	
 }
+
