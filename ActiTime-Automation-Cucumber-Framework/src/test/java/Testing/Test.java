77a5d8d0785159bf22434738b759de78cdc35907
NilavaPal <palnilava03@gmail.com>
2020-05-28 12:56:28 +0530

    Sample Class

--- ActiTime-Automation-Cucumber-Framework/src/test/java/Testing/Test.java
+++ ActiTime-Automation-Cucumber-Framework/src/test/java/Testing/Test.java
@@ -1,33 +1,30 @@
 package Testing;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
+import GenericUtils.CommonUtils;
 import GenericUtils.Driver;
 import GenericUtils.ExcelData;
 
 
 public class Test {
 
-//	public static void main(String[] args) throws EncryptedDocumentException, IOException, InvalidFormatException  {
-//		//String filepath="./resourceLib/testData/TestData.xlsx";
-//		
-//		ExcelData read= new ExcelData();
-//		String value=read.readExcelSheet("LoginSheet","LoginToActiTime","UserName");
-//		
-//		System.out.println(value);
-//		
-	@org.junit.Test
-	public void setUp() {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		Driver.launchBrowser();
-		Driver.driver.get("https://www.google.co.in/?gfe_rd=cr&ei=MzqTWKCwD7LY8gfU5J8I");
-	}
-
+		Properties pop = new Properties();
+		pop.load(new FileInputStream("./resourceLib/configuration/configFile.properties"));
+		CommonUtils common= new CommonUtils();
+		int statuscode=common.urlStatusCode(pop.getProperty("actiTime_URL"));
+		System.out.println(statuscode);
 		
-		
-
+		Driver.driver.get(pop.getProperty("actiTime_URL"));
 	}
+}
 
 
